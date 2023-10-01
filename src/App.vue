14c214f6c6bac7a7a83d73a40d1c5e882ea4419a
Jiacheng Wang <49028490+WangJiaCheng1999@users.noreply.github.com>
2023-10-01 21:19:49 +1100

    add tensor sans font and apply to whole app'

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,18 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Tensor Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+@font-face {
+    font-family: 'Tensor Sans';
+    src: url('../public/fonts/TenorSans-Regular.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
 }
 </style>
