cea39eed2a48e6d0c3650e0c6223dbf2eba7bf17
JIACHENG WANG <yojc1999@gmail.com>
2023-10-01 20:42:11 +1100

    create Navagation Bar's basic view

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,24 @@
+<!-- src/components/NavigationBar -->
+<template>
+    <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/register">Register</router-link>
+        <router-link to="/login">Login</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavigationBar',
+}
+</script>
+
+<style scoped>
+nav {
+    display: flex;
+    justify-content: space-around;
+    padding: 10px;
+    background-color: #eee;
+}    
+</style>
+
