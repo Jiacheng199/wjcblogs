c57c71df8638c1ae551d22df444fec602c71db85
Jiacheng Wang <49028490+WangJiaCheng1999@users.noreply.github.com>
2023-10-01 20:43:16 +1100

    Routing successfully between old vue views

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <NavigationBar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavigationBar from './components/NavigationBar.vue'
+
 export default {
   name: 'App',
+  components: {
+    NavigationBar
+  }
 }
 </script>
 
