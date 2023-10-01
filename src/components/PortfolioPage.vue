957fa6a44e69cf56f312d28d31fcc4205a3685b3
JIACHENG WANG <yojc1999@gmail.com>
2023-10-01 21:06:56 +1100

    add AboutMe,MyBlogs,PortfolioPage views and update router function

---
+++ src/components/PortfolioPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Portfolio</h1>
+        <p>Portfolio content goes here.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PortfolioPage',
+}
+</script>
+
+<style scoped>
+</style>
