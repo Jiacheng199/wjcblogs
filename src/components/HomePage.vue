1a90404b8249668e03bf7f02499bc92ef8295d0d
Jiacheng Wang <49028490+WangJiaCheng1999@users.noreply.github.com>
2023-10-01 20:33:44 +1100

    Create HomePage of wjcblos

---
+++ src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>Home page content goes here.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+}
+</script>
+
+<style scoped>    
+</style>
