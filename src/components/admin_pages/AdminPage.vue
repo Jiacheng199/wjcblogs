ddcb2f4c4df0eb27585f5f766e61b553037890a0
JIACHENG WANG <yojc1999@gmail.com>
2023-10-06 14:40:42 +1100

    add an empty vue templete for admin page

---
+++ src/components/admin_pages/AdminPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Admin Page</h1>
+        <p>Admin page goes here.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AdminPage',
+}
+</script>
+
+<style scoped>
+</style>
