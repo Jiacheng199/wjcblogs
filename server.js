58f436aa1bcdb4175b5b086bdcc924e936dfaa67
Jiacheng Wang <yojc1999@gmail.com>
2023-10-11 13:13:36 +1100

    Using sql pool to prevent auto-disconnected

--- server.js
+++ server.js
@@ -10,28 +10,32 @@ const crypto = require('crypto');
 app.use(bodyParser.json());
 app.use(cors());
 
-// MySQL connection 
-const connection = mysql.createConnection({
+// Create MySQL connection pool 
+const pool = mysql.createPool({
   host     : process.env.DB_HOST,
   user     : process.env.DB_USER,
   password : process.env.DB_PASS,
   database : process.env.DB_NAME,
   port     : process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect(function(err) {
+// Test the connection to the database
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error connecting: ' + err.stack);
     return;
   }
 
   console.log('Connected to the database.');
-
+  connection.release(); // Release the connection back to the pool
 });
 
-//read all blogs from mysql
+// Read all blogs from mysql
 app.get('/readblogs', (req, res) => {
-  connection.query('SELECT * FROM blogs', (error, results) => {
+  pool.query('SELECT * FROM blogs', (error, results) => {
     if (error) {
       console.error('Error querying: ' + error.stack);
       return res.status(500).json({ error });
@@ -40,9 +44,9 @@ app.get('/readblogs', (req, res) => {
   });
 });
 
-//read a single blog from mysql
+// Read a single blog from mysql
 app.get('/readblogs/:id', (req, res) => {
-  connection.query('SELECT * FROM blogs WHERE blog_id = ?', [req.params.id], (error, results) => {
+  pool.query('SELECT * FROM blogs WHERE blog_id = ?', [req.params.id], (error, results) => {
     if (error) {
       console.error('Error querying: ' + error.stack);
       return res.status(500).json({ error });
@@ -50,6 +54,5 @@ app.get('/readblogs/:id', (req, res) => {
     res.json(results);
   });
 });
-
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
