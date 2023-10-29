28a2f5bf27e1b130eae1b729cd5a583afdbc9fa4
Jiacheng Wang <yojc1999@gmail.com>
2023-10-29 11:28:15 +1100

    add a quill editor to admin page

--- src/components/admin_pages/AdminPage.vue
+++ src/components/admin_pages/AdminPage.vue
@@ -1,14 +1,28 @@
 <template>
     <div>
         <h1>Admin Page</h1>
+        <quill-editor v-model="editorHtml"></quill-editor>
         <p>Admin page goes here.</p>
     </div>
 </template>
 
 <script>
+import { quillEditor } from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'  // import styles
+import 'quill/dist/quill.bubble.css'
+
 export default {
     name: 'AdminPage',
-}//test
+    components: {
+        quillEditor
+    },
+    data() {
+        return {
+            editorHtml: ''
+        }
+    }
+}
 </script>
 
 <style scoped>
