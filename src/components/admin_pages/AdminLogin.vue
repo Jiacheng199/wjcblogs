ac80523860c8e619565f74e5cb45ab53993a95a0
Jiacheng199 <yojc1999@gmail.com>
2024-08-22 11:36:35 +0700

    change url to read call back uri from varible

--- src/components/admin_pages/AdminLogin.vue
+++ src/components/admin_pages/AdminLogin.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="admin-container">
         <h1>Admin Login</h1>
+        <h2>{{ callbackUrl }}</h2>
         <button @click="redirectToCognito">Login with Cognito</button>
     </div>
 </template>
@@ -8,12 +9,21 @@
 <script>
 export default {
     name: 'AdminLogin',
+    data(){
+        const prodRedirectUri = 'http://localhost:8080/adminpage';
+        const encodedRedirectUri = encodeURIComponent(prodRedirectUri);
+        return {
+            callbackUrl: encodedRedirectUri
+        };
+    },
     methods: {
         redirectToCognito() {
-
-            // Cognito Hosted UI URL
-            const url = 'https://auth.wjcblogs.com/oauth2/authorize?client_id=5p7d09j06sshpk7f3ge418kk1n&response_type=code&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fcallback';
-
+            
+            const devRedirectUri = 'http://localhost:8080/callback';
+            const encodedRedirectUri = encodeURIComponent(devRedirectUri);
+            const url = `https://auth.wjcblogs.com/oauth2/authorize?client_id=5p7d09j06sshpk7f3ge418kk1n&response_type=code&scope=email+openid+phone&redirect_uri=${encodedRedirectUri}`;
+            //const url = 'https://auth.wjcblogs.com/oauth2/authorize?client_id=5p7d09j06sshpk7f3ge418kk1n&response_type=code&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fadminpage';
+            
             // Redirect user to Cognito Hosted UI
             window.location.href = url;
         }
